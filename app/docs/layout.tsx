27460243552201553199356235d3c5645192ee50
FrexCheat <frexcheat@163.com>
2025-09-30 09:30:51 +0800

    chore: clean up meta.json pages of environment
    Signed-off-by: FrexCheat <frexcheat@163.com>

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,7 +1,9 @@
 import { DocsLayout, type DocsLayoutProps } from 'fumadocs-ui/layouts/docs';
-import { baseOptions } from '@/lib/layout.shared';
-import { source } from '@/lib/source';
 import { GithubInfo } from 'fumadocs-ui/components/github-info';
+import { FaBook, FaHome } from 'react-icons/fa';
+
+import { source } from '@/lib/source';
+import { baseOptions } from '@/lib/layout.shared';
 
 export default function Layout({ children }: LayoutProps<'/docs'>) {
   const options: DocsLayoutProps = {
@@ -11,6 +13,16 @@ export default function Layout({ children }: LayoutProps<'/docs'>) {
       {
         type: 'custom',
         children: <GithubInfo owner='FrexCheat' repo='DevBeginner-Doc' className='lg:-mx-2' />,
+      },
+      {
+        text: '返回首页',
+        url: '/',
+        icon: <FaHome />,
+      },
+      {
+        text: '软件仓库',
+        url: '/mirror',
+        icon: <FaBook />,
       },
     ],
   };
